2f5df9e179ea6f52d4f3fa79543b83c38cb77ca3
Varun Joshi <joshvarun@gmail.com>
2020-08-18 08:51:05 +0530

    Create Auth context, login register routes

--- client/src/App.js
+++ client/src/App.js
@@ -6,22 +6,29 @@ import Navbar from './components/layout/Navbar';
 import { Home } from './components/pages/Home';
 import { About } from './components/pages/About';
 import ContactState from './context/Contact/ContactState';
+import AuthState from './context/Auth/AuthState';
+import { Register } from './components/auth/Register';
+import { Login } from './components/auth/Login';
 
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <div className='container'>
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route exact path='/about' component={About} />
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <Router>
+          <Fragment>
+            <Navbar />
+            <div className='container'>
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/about' component={About} />
+                <Route exact path='/register' component={Register} />
+                <Route exact path='/login' component={Login} />
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
+      </ContactState>
+    </AuthState>
   );
 };
 
